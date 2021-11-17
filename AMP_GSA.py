410f846a95834907d563d5c2c9565da7a86b085a
MikhailButs <misha.bucz@mail.ru>
2021-11-18 02:27:40 +0700

    Initial commit
    Signed-off-by: MikhailButs <misha.bucz@mail.ru>

---
+++ AMP_GSA.py
@@ -0,0 +1,23 @@
+class AMP_GSA:
+    def __init__(self):
+        self.gainCH1 = None
+        self.gainCH2 = None
+        self.conf = None
+
+    def get_name(self):
+        pass
+
+    def get_conf(self):
+        pass
+
+    def set_conf(self):
+        pass
+
+    def set_gainCH1(self):
+        pass
+
+    def set_gainCH2(self):
+        pass
+
+    def make_call(self):
+        pass
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 # main entrance point
+import socket
+
+import AMP_GSA
 
 if __name__ == '__main__':
-    print('hello world')
+    address = ('127.0.0.1', 10001)
+    sock = socket.socket()
+    sock.connect(address)
+
+    gsa = AMP_GSA.AMP_GSA()
