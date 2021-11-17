61bdeafad7704d6ec36db76f6771a03a68810901
MikhailButs <misha.bucz@mail.ru>
2021-11-18 05:26:52 +0700

    Initial commit
    Signed-off-by: MikhailButs <misha.bucz@mail.ru>

--- AMP_GSA.py
+++ AMP_GSA.py
@@ -1,23 +1,30 @@
 class AMP_GSA:
-    def __init__(self):
+    def __init__(self, sock):
         self.gainCH1 = None
         self.gainCH2 = None
         self.conf = None
+        self.sock = sock
 
     def get_name(self):
-        pass
+        self.sock.send('*IDN?\n'.encode('utf-8'))
+        return self.sock.recv(1024).decode('utf-8')[1:-1]
 
     def get_conf(self):
-        pass
+        self.sock.send('*CONF?\n'.encode('utf-8'))
+        return int(self.sock.recv(1024).decode('utf-8')[1:-1])
 
-    def set_conf(self):
-        pass
+    def set_conf(self, conf):
+        self.sock.send(f'*CONF {conf}\n'.encode('utf-8'))
+        return self.sock.recv(1024).decode('utf-8')[1:-1]
 
-    def set_gainCH1(self):
-        pass
+    def set_gainCH1(self, gainCH1):
+        self.sock.send(f'*GAIN A {gainCH1}\n'.encode('utf-8'))
+        return self.sock.recv(1024).decode('utf-8')[1:-1]
 
-    def set_gainCH2(self):
-        pass
+    def set_gainCH2(self, gainCH2):
+        self.sock.send(f'*GAIN B {gainCH2}\n'.encode('utf-8'))
+        return self.sock.recv(1024).decode('utf-8')[1:-1]
 
-    def make_call(self):
-        pass
+    def make_call(self, num, amp, impulse, pause):
+        self.sock.send(f'*CAL {num} {amp} {impulse} {pause}\n'.encode('utf-8'))
+        return self.sock.recv(1024).decode('utf-8')[1:-1]
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # main entrance point
 import socket
+import time
 
 import AMP_GSA
 
@@ -8,4 +9,31 @@ if __name__ == '__main__':
     sock = socket.socket()
     sock.connect(address)
 
-    gsa = AMP_GSA.AMP_GSA()
+    gsa = AMP_GSA.AMP_GSA(sock)
+    print('asked: NAME')
+    print(gsa.get_name())
+    time.sleep(5)
+
+    print('asked: CONF?')
+    print(gsa.get_conf())
+    time.sleep(5)
+
+    print('set : CONF')
+    print(gsa.set_conf(9))
+    time.sleep(5)
+
+    print('set : GAIN CH1')
+    print(gsa.set_gainCH1(10))
+    time.sleep(5)
+
+    print('set : GAIN CH2')
+    print(gsa.set_gainCH2(11))
+    time.sleep(5)
+
+    print('make : CAL')
+    print(gsa.make_call(10, 10, 10, 10))
+
+    print('close connection')
+    time.sleep(10)
+
+    sock.close()
