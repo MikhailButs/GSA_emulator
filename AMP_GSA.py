5165bd04d3dd898ee7564c87baedf33531e37ea6
misha <misha.bucz@mail.ru>
2021-12-09 02:21:48 +0300

    Merge remote-tracking branch 'origin/AMP&GSAclass' into AMP&GSAclass

--- AMP_GSA.py
+++ AMP_GSA.py
@@ -3,44 +3,65 @@ import socket
 
 
 class AMP_GSA:
-    def __init__(self, address):
-        self.gainCH1 = None
-        self.gainCH2 = None
-        self.conf = None
-        self.client = Client(address)
+    def __init__(self, conffilename='conf'):
+        configfile = open(conffilename, 'r')
+        lines = configfile.readlines()
+        conf_dict = {i[:i.index(' ')]: i[i.index(' ') + 1:-1] for i in lines}
+        self.gainCH1 = conf_dict.setdefault('gainCH1(8bit)', '0')
+        self.gainCH2 = conf_dict.setdefault('gainCH2(8bit)', '0')
+        self.conf = conf_dict.setdefault('T(5bit)', '0')
+        self.num_pulses = conf_dict.setdefault('num_pulses(16bit)', '0')
+        self.amp_pulses = conf_dict.setdefault('amp_pulses(16bit)', '0')
+        self.width_pulses = conf_dict.setdefault('width_pulses(8bit)', '0')
+        self.width_pause = conf_dict.setdefault('width_pause(8bit)', '0')
+        self.name = conf_dict.setdefault('name', 'name')
 
     def get_name(self):
-        return self.client.ask('IDN?')
+        return '*IDN?\n'
 
     def get_conf(self):
-        return self.client.ask('CONF?')
+        return '*CONF?\n'
 
     def set_conf(self, conf):
-        return self.client.ask(f'CONF {conf}')
+        return f'*CONF {conf}\n'
 
     def set_gainCH1(self, gainCH1):
-        return self.client.ask(f'GAIN A {gainCH1}')
+        return f'*GAIN A {gainCH1}\n'
 
     def set_gainCH2(self, gainCH2):
-        return self.client.ask(f'GAIN B {gainCH2}')
+        return f'*GAIN B {gainCH2}\n'
 
     def make_call(self, num, amp, impulse, pause):
-        return self.client.ask(f'CAL {num} {amp} {impulse} {pause}')
+        return f'*CAL {num} {amp} {impulse} {pause}\n'
+
+    def refresh_conf(self, conffilename='conf'):
+        conffilename = open(conffilename, 'w')
+        conffilename.write(f'name {self.name}\n')
+        conffilename.write(f'T(5bit) {self.conf}\n')
+        conffilename.write(f'gainCH1(8bit) {self.gainCH1}\n')
+        conffilename.write(f'gainCH2(8bit) {self.gainCH2}\n')
+        conffilename.write(f'num_pulses(16bit) {self.num_pulses}\n')
+        conffilename.write(f'amp_pulses(16bit) {self.amp_pulses}\n')
+        conffilename.write(f'width_pulses(8bit) {self.width_pulses}\n')
+        conffilename.write(f'width_pause(8bit) {self.width_pause}\n')
+        conffilename.close()
 
 
 class Client:
-    def __init__(self, address):
+    def __init__(self, address=('127.0.0.1', 10001)):
         self.address = address
         self.sock = socket.socket()
         self.sock.connect(address)
 
     def ask(self, quest):
-        self.sock.send(f'*{quest}\n'.encode('utf-8'))
-        return self.sock.recv(1024).decode('utf-8')[1:-1]
+        self.sock.send(f'{quest}'.encode('utf-8'))
+        return self.sock.recv(1024).decode('utf-8')
 
     def __del__(self):
         self.sock.close()
 
-def sys_print(*data):
+'''' 
+    def sys_print(*data):
     sys.stdout.write(f'[{datetime.datetime.now().strftime("%H:%M:%S")}] {"".join(map(str, data))}')
     return 'printed'
+'''
--- ui_gsa.py
+++ ui_gsa.py
@@ -2,6 +2,8 @@ import sys  # sys нужен для передачи argv в QApplication
 from PyQt5 import QtWidgets
 import ui_design  # Это наш конвертированный файл дизайна
 import AMP_GSA
+import datetime
+
 
 class ExampleApp(QtWidgets.QMainWindow, ui_design.Ui_win):
     def __init__(self):
@@ -11,13 +13,17 @@ class ExampleApp(QtWidgets.QMainWindow, ui_design.Ui_win):
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
 
         # блок "usb-eternet"
-        self.amp = None
+        self.amp = AMP_GSA.AMP_GSA()
+        self.ether_client = None
         self.ether_con_btn.clicked.connect(self.ether_con)
+        self.get_name_btn_ether.clicked.connect(self.make_get_name)
 
         # блок "постоянная времени"
         self.time_const_slider.valueChanged.connect(self.set_time_const_spin_box)
         self.time_const_spin_box.valueChanged.connect(self.set_time_const_slider)
         self.setconf_btn.clicked.connect(self.set_conf)
+        self.time_const_spin_box.setValue(float(self.amp.conf))
+        self.getconf_btn.clicked.connect(self.get_conf)
 
         # блок "коэффициент усиления"
         self.setgain_slider.valueChanged.connect(self.set_setgain_spin)
@@ -26,6 +32,8 @@ class ExampleApp(QtWidgets.QMainWindow, ui_design.Ui_win):
         self.setgain_spin_2.valueChanged.connect(self.set_setgain_slider_2)
         self.setgain_btn.clicked.connect(self.set_gain_A)
         self.setgain_btn_2.clicked.connect(self.set_gain_B)
+        self.setgain_spin.setValue(float(self.amp.gainCH1))
+        self.setgain_spin_2.setValue(float(self.amp.gainCH2))
 
         # блок "калибровка (гса)"
         self.set_num_pulses_slider.valueChanged.connect(self.set_set_num_pulses_spin)
@@ -37,86 +45,140 @@ class ExampleApp(QtWidgets.QMainWindow, ui_design.Ui_win):
         self.set_pause_slider.valueChanged.connect(self.set_set_pause_spin)
         self.set_pause_spin.valueChanged.connect(self.set_set_pause_slider)
         self.start_call.clicked.connect(self.make_start_call)
-
-        # блок "eternet"
-        self.get_name_btn_ether.clicked.connect(self.make_get_name)
+        self.set_num_pulses_spin.setValue(float(self.amp.num_pulses))
+        self.set_amp_pulses_spin.setValue(float(self.amp.amp_pulses))
+        self.set_time_pulse_spin.setValue(float(self.amp.width_pulses))
+        self.set_pause_spin.setValue(float(self.amp.width_pause))
 
     # блок "usb-eterne"
     def ether_con(self):
         if len(self.ip_field.text()) & len(self.port_field.text()) != 0:
-            self.cml_box.append('CONNECTION')
-            self.amp = AMP_GSA.AMP_GSA((str(self.ip_field.text()), int(self.port_field.text())))
+            self.ether_client = AMP_GSA.Client((str(self.ip_field.text()), int(self.port_field.text())))
+            self.cml_box.append(time_print(f'CONNECTED TO ({self.ip_field.text()}, {self.port_field.text()})'))
 
+    def make_get_name(self):
+        if self.ether_client is not None:
+            self.cml_box.append(time_print(f'ASKED {self.amp.get_name()}'))
+            name = self.ether_client.ask(self.amp.get_name())
+            self.amp.name = name[1:-1]
+            self.amp.refresh_conf()
+            self.cml_box.append(time_print(f'RECEIVED {name}'))
 
     # блок "постоянная времени"
     def set_time_const_spin_box(self):
         self.time_const_spin_box.setValue(self.time_const_slider.value())
+        self.amp.conf = str(int(self.time_const_slider.value()))
+        self.amp.refresh_conf()
 
     def set_time_const_slider(self):
         self.time_const_slider.setValue(self.time_const_spin_box.value())
+        self.amp.conf = str(int(self.time_const_spin_box.value()))
+        self.amp.refresh_conf()
 
     def set_conf(self):
-        self.cml_box.append('CONF')
+        if self.ether_client is not None:
+            self.cml_box.append(time_print(f'ASKED {self.amp.set_conf(self.amp.conf)}'))
+            self.cml_box.append(time_print(f'RECEIVED {self.ether_client.ask(self.amp.set_conf(self.amp.conf))}'))
+
+    def get_conf(self):
+        if self.ether_client is not None:
+            self.cml_box.append(time_print(f'ASKED {self.amp.get_conf()}'))
+            conf = self.ether_client.ask(self.amp.get_conf())
+            self.amp.conf = conf[1:-1]
+            self.amp.refresh_conf()
+            self.time_const_spin_box.setValue(float(self.amp.conf))
+            self.cml_box.append(time_print(f'RECEIVED {conf}'))
 
     # блок "коэффициент усиления"
     def set_setgain_slider(self):
         self.setgain_slider.setValue(self.setgain_spin.value())
+        self.amp.gainCH1 = str(int(self.setgain_spin.value()))
+        self.amp.refresh_conf()
 
     def set_setgain_spin(self):
         self.setgain_spin.setValue(self.setgain_slider.value())
+        self.amp.gainCH1 = str(int(self.setgain_spin.value()))
+        self.amp.refresh_conf()
 
     def set_setgain_slider_2(self):
         self.setgain_slider_2.setValue(self.setgain_spin_2.value())
+        self.amp.gainCH2 = str(int(self.setgain_spin_2.value()))
+        self.amp.refresh_conf()
 
     def set_setgain_spin_2(self):
         self.setgain_spin_2.setValue(self.setgain_slider_2.value())
+        self.amp.gainCH2 = str(int(self.setgain_spin_2.value()))
+        self.amp.refresh_conf()
 
     def set_gain_A(self):
-        self.cml_box.append('GAIN A')
+        if self.ether_client is not None:
+            self.cml_box.append(time_print(f'ASKED {self.amp.set_gainCH1(self.amp.gainCH1)}'))
+            self.cml_box.append(time_print(f'RECEIVED {self.ether_client.ask(self.amp.set_gainCH1(self.amp.gainCH1))}'))
 
     def set_gain_B(self):
-        self.cml_box.append('GAIN B')
+        if self.ether_client is not None:
+            self.cml_box.append(time_print(f'ASKED {self.amp.set_gainCH2(self.amp.gainCH2)}'))
+            self.cml_box.append(time_print(f'RECEIVED {self.ether_client.ask(self.amp.set_gainCH2(self.amp.gainCH2))}'))
 
     # блок "калибровка (гса)"
     def set_set_num_pulses_spin(self):
         self.set_num_pulses_spin.setValue(self.set_num_pulses_slider.value())
+        self.amp.num_pulses = str(int(self.set_num_pulses_spin.value()))
+        self.amp.refresh_conf()
 
     def set_set_num_pulses_slider(self):
         self.set_num_pulses_slider.setValue(self.set_num_pulses_spin.value())
+        self.amp.num_pulses = str(int(self.set_num_pulses_spin.value()))
+        self.amp.refresh_conf()
 
     def set_set_amp_pulses_spin(self):
         self.set_amp_pulses_spin.setValue(self.set_amp_pulses_slider.value())
+        self.amp.amp_pulses = str(int(self.set_amp_pulses_spin.value()))
+        self.amp.refresh_conf()
 
     def set_set_amp_pulses_slider(self):
         self.set_amp_pulses_slider.setValue(self.set_amp_pulses_spin.value())
+        self.amp.amp_pulses = str(int(self.set_amp_pulses_spin.value()))
+        self.amp.refresh_conf()
 
     def set_set_time_pulse_spin(self):
         self.set_time_pulse_spin.setValue(self.set_time_pulse_slider.value())
+        self.amp.width_pulses = str(int(self.set_time_pulse_spin.value()))
+        self.amp.refresh_conf()
 
     def set_set_time_pulse_slider(self):
         self.set_time_pulse_slider.setValue(self.set_time_pulse_spin.value())
+        self.amp.width_pulses = str(int(self.set_time_pulse_spin.value()))
+        self.amp.refresh_conf()
 
     def set_set_pause_spin(self):
         self.set_pause_spin.setValue(self.set_pause_slider.value())
+        self.amp.width_pause = str(int(self.set_pause_spin.value()))
+        self.amp.refresh_conf()
 
     def set_set_pause_slider(self):
         self.set_pause_slider.setValue(self.set_pause_spin.value())
+        self.amp.width_pause = str(int(self.set_pause_spin.value()))
+        self.amp.refresh_conf()
 
     def make_start_call(self):
-        if self.amp != None:
-            self.amp.make_call()
+        if self.ether_client is not None:
+            a, b, c, d = self.amp.num_pulses, self.amp.amp_pulses, self.amp.width_pulses, self.amp.width_pause
+            self.cml_box.append(time_print(f'ASKED {self.amp.make_call(a, b, c, d)}'))
+            self.cml_box.append(time_print(f'RECEIVED {self.ether_client.ask(self.amp.make_call(a, b, c, d))}'))
 
-    # блок "eternet"
-    def make_get_name(self):
-        self.cml_box.append('CONF?')
 
-    # вывод в cml
+def time_print(data):
+    data = data.replace('\n', '\\n')
+    return f'[{datetime.datetime.now().strftime("%H:%M:%S")}] {data}'
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
     window = ExampleApp()  # Создаём объект класса ExampleApp
     window.show()  # Показываем окно
     app.exec_()  # и запускаем приложение
+
 
 if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
     main()  # то запускаем функцию main()
