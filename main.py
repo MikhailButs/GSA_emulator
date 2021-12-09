4a804c3b6d84452e53711af49e716a88e9189caf
misha <misha.bucz@mail.ru>
2021-12-09 17:02:44 +0300

    Merge remote-tracking branch 'origin/AMP&GSAclass' into AMP&GSAclass

--- main.py
+++ main.py
@@ -1,40 +1,6 @@
 # main entrance point
-# Тест команд класса AMP_GSA
-import socket
-import time
-import AMP_GSA
+
 import ui_gsa
 
 if __name__ == '__main__':
     ui_gsa.main()
-
-    '''print('Test of AMP_GSA starts')
-    address = ('127.0.0.1', 10001)
-    print(f'Connected with {address}\n')
-
-    gsa = AMP_GSA.AMP_GSA(address)
-    print('asked: NAME')
-    print('Answered ', gsa.get_name(), '\n')
-    time.sleep(5)
-
-    print('asked: CONF?')
-    print('Answered ', gsa.get_conf(), '\n')
-    time.sleep(5)
-
-    print('set : CONF')
-    print('Set ', gsa.set_conf(9), '\n')
-    time.sleep(5)
-
-    print('set : GAIN CH1')
-    print('Set ', gsa.set_gainCH1(10), '\n')
-    time.sleep(5)
-
-    print('set : GAIN CH2')
-    print('Set ', gsa.set_gainCH2(11), '\n')
-    time.sleep(5)
-
-    print('make : CAL')
-    print('made ', gsa.make_call(10, 10, 10, 10), '\n')
-
-    print('close connection')
-    time.sleep(10)'''
